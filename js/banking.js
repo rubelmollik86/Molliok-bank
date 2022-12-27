7a91919a10abebe3ada913b83f857d44971a4980
rubelmollik86 <rubelmollik86@gmail.com>
2022-12-27 23:22:52 +0600

    Balance updatte

--- js/banking.js
+++ js/banking.js
@@ -2,11 +2,26 @@
 document.getElementById('Deposite-Button').addEventListener('click', function () {
     // Get the amount deposited
     const depositeInput = document.getElementById('Input-Deposite');
-    const depositeAmount = depositeInput.value;
-    console.log(depositeAmount);
+    const depositeAmountText = depositeInput.value;
+    const depositeAmount = parseFloat(depositeAmountText);
+    // console.log(depositeAmount);
+// update deposite total
+    const CurrentTotal = document.getElementById('Deposite-Total');
 
-    const CurrentDepositeTotal = document.getElementById('Deposite-Total');
-    CurrentDepositeTotal.innerText = depositeAmount;
+    // const currentDepositeAmount = CurrentTotal.innerText;
+    const CurrentTotalAmountText = CurrentTotal.innerText;
+    const currentDepositeAmount = parseFloat(CurrentTotalAmountText)
+    const NewDepositeTotal = currentDepositeAmount + depositeAmount;
+
+    CurrentTotal.innerText = NewDepositeTotal;
+
+    // update Balance
+    const BalanceUpdate = document.getElementById('Balance-Update');
+    const balanceUpdateText = BalanceUpdate.innerText;
+    const previousBalance = parseFloat(balanceUpdateText);
+    const NewBlanceTotal = previousBalance + depositeAmount;
+    BalanceUpdate.innerText = NewBlanceTotal;
+
 
     // clear the deposite input fild
     depositeInput.value = '';
